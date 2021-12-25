8d780a1856e0406740463e20a74ffddd940c0366
xinshu hua <work.hua.xinshu@gmail.com>
2021-12-26 00:55:42 +0100

    add comment in edge.js

--- edge.js
+++ edge.js
@@ -1,19 +1,20 @@
 
-
 class Edge {
 
     /**
-     * @param {Vertex} sour_vertex
-     * @param {Vertex} dest_vertex
+     * @param {Number} sour_vertex_id
+     * @param {Number} dest_vertex_id
      * @param {Object} edge_property
+     * @param {Boolean} is_multi_edge
      */
      constructor(sour_vertex_id, dest_vertex_id, edge_property, is_multi_edge=false){
         this.sour_vertex_id = sour_vertex_id;
         this.dest_vertex_id = dest_vertex_id;
         this.property = edge_property;
+        
+        //is_multi_edge flag indicates whether this edge is merged from multiple edges
         this.is_multi_edge = is_multi_edge;
     }
-
 }
 
 export {Edge} 
