8b8ab41c95962ed04b40dc05ec6f5f6038f0ae08
xinshu hua <work.hua.xinshu@gmail.com>
2021-12-26 01:02:04 +0100

    add comment in vertex.js

--- vertex.js
+++ vertex.js
@@ -1,35 +1,26 @@
-import {Graph} from './graph.js';
-
+import { Graph } from './graph.js';
 
 class Vertex {
 
-    /**
-     * 
-     * @param {Number} vertex_id 
-     * @param {Object} vertex_property 
-     */
-    constructor(vertex_id, vertex_property={}, is_multi_vertex=false, sub_vertices=[], edges=[]){
-        this.property = vertex_property;
-        this.id = vertex_id;
-
-        //set is_multi_vertex flag as false
-        this.is_multi_vertex = is_multi_vertex;
-        this.sub_vertices = sub_vertices;
-        this.edges = edges;
-    }
-
-
-    add_sub_vertex(v){
-        this.sub_vertices.push(v);
-    }
-
-
-
+  /**
+   * 
+   * @param { Number } vertex_id 
+   * @param { Object } vertex_property 
+   * @param { Boolean } is_multi_vertex
+   * @param { Array } sub_vertices
+   * @param { Array } edges
+   */
+  constructor(vertex_id, vertex_property = {}, is_multi_vertex = false, sub_vertices = [], edges = []) {
+    this.property = vertex_property;
+    this.id = vertex_id;  
+    //set is_multi_vertex flag as false
+    this.is_multi_vertex = is_multi_vertex;
+    this.sub_vertices = sub_vertices;
+    this.edges = edges;
+  }  
+  add_sub_vertex(v) {
+    this.sub_vertices.push(v);
+  }
 }
 
-
-
-
-
-
-export {Vertex}
+export { Vertex }
